5a097e96a616ab60c981bca26f1a2199c5bd685f
dandycomp <dandycomp@gmail.com>
2015-02-16 16:50:49 +0100

    wersja do promotora i recenzenta

--- src/parameters.h
+++ src/parameters.h
@@ -11,8 +11,6 @@ static const int IMAGE_HEIGHT_ = 1000;//wysokosc obrazu
 static const int PATCH_SIZE = 25;// rozmiar patcha
 static const int SUB_IMG = 35;// rozmiar obszaru przeszukiwan
 
-//static const int PATCH_SIZE = 13;// rozmiar patcha
-//static const int SUB_IMG = 15;// rozmiar obszaru przeszukiwan
 static const int RESIZE_COEFF = 3; // wspolczynnik skalowania w interpolacji
 
 static const int IMAGE_WIDTH = IMAGE_WIDTH_*RESIZE_COEFF;
